c303a4c1af13e0bd362656acba9a7912174c368b
chj <2741635790@qq.com>
2023-12-08 16:00:07 +0800

    一点小修改

--- src/com/chj/code/utils/MessageAppenderFrame.java
+++ src/com/chj/code/utils/MessageAppenderFrame.java
@@ -21,33 +21,10 @@ public class MessageAppenderFrame extends JFrame {
         // 将文本区域添加到窗口
         getContentPane().add(scrollPane, BorderLayout.CENTER);
 
-        // 创建一个面板用于放置输入框和发送按钮
-//        JPanel inputPanel = new JPanel();
-//        inputPanel.setLayout(new BorderLayout());
-
-//        JTextField inputField = new JTextField();
-//        JButton appendButton = new JButton("Append Message");
-
-        // 添加按钮的点击事件监听器
-//        appendButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                appendMessage(inputField.getText());
-//            }
-//        });
-
-        // 将输入框和按钮添加到输入面板
-//        inputPanel.add(inputField, BorderLayout.CENTER);
-//        inputPanel.add(appendButton, BorderLayout.EAST);
-
-        // 将输入面板添加到窗口底部
-//        getContentPane().add(inputPanel, BorderLayout.SOUTH);
-
         // 设置窗口大小和关闭操作
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //setLocationRelativeTo(null); // 居中显示窗口
     }
 
     public static void appendMessage(String message) {
